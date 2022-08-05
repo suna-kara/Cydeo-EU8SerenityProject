d478c6d209e81524578025ec7daaffc3057a20ce
Suna Kara <karacalisuna912@gmail.com>
2022-08-05 12:55:31 +0300

    Ensure.that added

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -3,12 +3,15 @@ package eu8.spartan.admin;
 
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 import static net.serenitybdd.rest.SerenityRest.given;
 import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static org.hamcrest.Matchers.is;
 
 @SerenityTest
 public class SpartanAdminGetTest {
@@ -60,4 +63,25 @@ public class SpartanAdminGetTest {
         System.out.println("name = " + name);
     }
 
-}
+    @DisplayName("GET request with Serenity Assertion way")
+    @Test
+    public void getOneSpartanAssertion() {
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin", "admin")
+                .pathParam("id", 15)
+                .when()
+                .get("/api/spartans/{id}");
+
+        //Serenity way of assertion
+
+        Ensure.that("Status code is 200", validatableResponse -> validatableResponse.statusCode(201));
+
+        Ensure.that("Content-type is JSON", vRes -> vRes.contentType(ContentType.JSON));
+        Ensure.that("Id is 15",vRes -> vRes.body("id",is(15)));
+
+    }
+
+    }
